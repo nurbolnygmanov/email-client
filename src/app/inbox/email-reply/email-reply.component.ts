1b1c41bb25d90e1c8899ee7f2441d400a5fbbabe
nurbol <nurbolnygmanov@gmail.com>
2023-06-27 10:18:29 +0200

    finish email reply

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EmailDetails, EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
 export class EmailReplyComponent {
+  showModal = false;
+  @Input() email!: EmailDetails;
 
+  constructor(private emailService: EmailService) {}
+
+  ngOnInit() {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `Re: ${this.email.subject}`,
+      text: `\n\n\n --- ${this.email.from} wrote:\n ${this.email.text}`,
+    };
+  }
+
+  onSubmit(email: EmailDetails) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
